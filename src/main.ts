889c151e3b97aeb7ec61591562bd310cea273c68
Cihan-Frupro <cihan.eksioglu@frupro.com>
2024-07-03 22:58:44 +0300

    CORS settings added

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+const CORS_ORIGINS = process.env.CORS_ORIGINS.split(',');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.enableCors({
+    origin: CORS_ORIGINS,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  console.log(configService.get('PORT'));
   await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
