088e4e2ca76af16d745ebb338f26a042425bb2b0
Cihan-Frupro <cihan.eksioglu@frupro.com>
2024-07-06 01:12:05 +0300

    login endpoint added - jwt / cookie

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,25 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: Number(configService.get('JWT_EXPIRATION_TIME')),
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import { User } from 'src/users/entities/user.entity';
+import * as moment from 'moment';
+import { TokenPayload } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const jwtExpiration = this.configService.get('JWT_EXPIRATION_TIME');
+    const expires = moment().add(jwtExpiration, 's').toDate();
+    console.log('expires', expires);
+
+    const tokenPayload: TokenPayload = {
+      _id: user._id.toHexString(),
+      email: user.email,
+    };
+
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires: expires,
+    });
+
+    response.send({
+      token,
+      success: true,
+      message: 'Login successful',
+    });
+  }
+}
